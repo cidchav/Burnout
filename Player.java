dd4795afb739649c9ce14e3de0c802a999a74a08
cidchav <b2027cichavez@pshs.edu.ph>
2024-12-18 01:51:54 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,27 +1,60 @@
-public class Player {
-    private int mentalHP, physicalHP, emotionalHP;
-    private Location location;
+package burnout;
+
+public class Player extends Student {
+    private String specialty;
+    private int mentalHP;
+    private int physicalHP;
+    private int emotionalHP;
     
-    public Player(int mHP, int pHP, int eHP, Location l){
-        mentalHP = mHP;
-        physicalHP = pHP;
-        emotionalHP = eHP;
-        location = l;
+    public Player(String name, Location location, String dialogue, String section, 
+            String specialty, int mentalHP, int physicalHP, int emotionalHP){
+        super(name, location, dialogue, section);
+        this.mentalHP = mentalHP;
+        this.physicalHP = physicalHP;
+        this.emotionalHP = emotionalHP;
     }
     
-    public int getMentalHP(){
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public int getMentalHP() {
         return mentalHP;
     }
-    public int getPhysicalHP(){
+
+    public int getPhysicalHP() {
         return physicalHP;
     }
-    public int getEmotionalHP(){
+
+    public int getEmotionalHP() {
         return emotionalHP;
     }
-    public Location getLocation(){
-        return location; 
+    
+    public int addMentalHP(int addend) {
+        return mentalHP;
     }
-    public void setLocation(Location l){
-        this.location = l; 
+
+    public int addPhysicalHP(int addend) {
+        return physicalHP;
+    }
+
+    public int addEmotionalHP(int addend) {
+        return emotionalHP;
+    }
+    
+    public void inspect(Interactive interactive){
+        interactive.interact();
+    }
+    
+    @Override
+    public void study(int hours){
+        super.study(hours);
+        mentalHP =+ hours;
+    }
+    
+    @Override
+    public void takeTest(Test test){
+        super.takeTest(test);
+        //INSERT CASE 3 HERE!
     }
 }
