bb36c28bc34df63d51429ff57f4cef9f50096416
seananabread <seevi2728@gmail.com>
2024-12-17 21:44:28 +0800

    Update main.java

--- main.java
+++ main.java
@@ -3,7 +3,27 @@ package burnout;
 public class Burnout {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        Location classroom = new Location("Classroom");
+        Location library = new Location("Library");
+        Player player = new Player(10, 10, 10, classroom);
+        
+        System.out.println(player.getLocation().getName());
+        System.out.println("Where should I spend my time?");
+        System.out.println("> Classroom");
+        System.out.println("> Library");
+        String locationInput = sc.nextLine();
+        
+        switch(locationInput){
+            case "Classroom":
+                player.setLocation(classroom);
+                break;    
+            case "Library":
+                player.setLocation(library);
+                break;  
+        }
+        
+        System.out.println(player.getLocation().getName());
     }
-    
 }
