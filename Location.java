a531a7dc03a5e0bfcd7d055ac767c3a01a5c53d1
cidchav <b2027cichavez@pshs.edu.ph>
2024-12-18 01:52:38 +0800

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,11 +1,45 @@
-public class Location{
-    private String name;
+package burnout;
+import java.util.ArrayList;
+
+public class Location implements Interactive {
+    private String name, activity;
+    private ArrayList<Person> peopleList;
     
-    public Location(String n){
-        name = n;
+    public Location(String name, String activity){
+        this.name = name;
+        this.activity = activity;
     }
     
-    public String getName(){
+    public String getName() {
         return name;
     }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public ArrayList<Person> getPeopleList() {
+        return peopleList;
+    }
+    
+    public void addPerson(Person person){
+        getPeopleList().add(person);
+    }
+    
+    public void removePerson(Person person){
+        getPeopleList().remove(person);
+    }
+    
+    @Override
+    public void interact(){
+        System.out.println("The activity here is "+activity+".");
+    }
+    
+    /*
+    Has a name
+    Has a special activity
+    Has list of people
+    Is interactive
+    */
+
 }
