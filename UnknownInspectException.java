c223fc544bcbd83ccb3d3e822b9c1634dbdf40a2
seananabread <seevi2728@gmail.com>
2024-12-18 19:23:37 +0800

    Create UnknownInspectException.java

---
+++ UnknownInspectException.java
@@ -0,0 +1,10 @@
+package q2aa2_tau_chavez_sevilla;
+
+public class UnknownInspectException extends Exception {
+    public UnknownInspectException(){
+        super();
+    }
+    public UnknownInspectException(String message){
+        super(message);
+    }   
+}
