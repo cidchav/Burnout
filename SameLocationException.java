bf0327e04834173b91990d0c1bd3b1df7153b73f
seananabread <seevi2728@gmail.com>
2024-12-18 19:23:22 +0800

    Create SameLocationException.java

---
+++ SameLocationException.java
@@ -0,0 +1,10 @@
+package q2aa2_tau_chavez_sevilla;
+
+public class SameLocationException extends Exception {
+    public SameLocationException(){
+        super();
+    }
+    public SameLocationException(String message){
+        super(message);
+    }
+}
