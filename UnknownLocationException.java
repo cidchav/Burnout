32c56e7aa99fb72f3c0fd3b66c721fb81b19535d
seananabread <seevi2728@gmail.com>
2024-12-18 19:23:54 +0800

    Create UnknownLocationException.java

---
+++ UnknownLocationException.java
@@ -0,0 +1,10 @@
+package q2aa2_tau_chavez_sevilla;
+
+public class UnknownLocationException extends Exception {
+    public UnknownLocationException(){
+        super();
+    } 
+    public UnknownLocationException(String message){
+        super(message);
+    }
+}
