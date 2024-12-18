f1784bbee1b0314777f70f2e1932e750ef43317e
seananabread <seevi2728@gmail.com>
2024-12-18 19:20:48 +0800

    Update Location.java

--- Location.java
+++ Location.java
@@ -1,45 +1,34 @@
-package q2aa_tau_chavez_sevilla;
+package q2aa2_tau_chavez_sevilla;
+
 import java.util.ArrayList;
 
-public class Location implements Interactive {
+public class Location implements Interactive{
     private String name, activity;
     private ArrayList<Person> peopleList;
+    protected static ArrayList<Location> locationList = new ArrayList<Location>();
     
-    public Location(String name, String activity){
+    public Location(String name){ //, String activity
         this.name = name;
-        this.activity = activity;
-    }
-    
+        locationList.add(this);
+        //this.activity = activity;
+    } 
     public String getName() {
         return name;
     }
-
     public String getActivity() {
         return activity;
     }
-
     public ArrayList<Person> getPeopleList() {
         return peopleList;
     }
-    
     public void addPerson(Person person){
         getPeopleList().add(person);
     }
-    
     public void removePerson(Person person){
         getPeopleList().remove(person);
-    }
-    
+    } 
     @Override
     public void interact(){
         System.out.println("The activity here is "+activity+".");
     }
-    
-    /*
-    Has a name
-    Has a special activity
-    Has list of people
-    Is interactive
-    */
-
 }
