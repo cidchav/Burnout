b9a5ab12d99f83bf114aac42f1657fb3fdf19d97
cidchav <b2027cichavez@pshs.edu.ph>
2024-12-18 22:11:30 +0800

    Update NonPlayableStudent.java
    changed to aa2

--- NonPlayableStudent.java
+++ NonPlayableStudent.java
@@ -1,4 +1,4 @@
-package q2aa_tau_chavez_sevilla;
+package q2aa2_tau_chavez_sevilla;
 
 public class NonPlayableStudent extends Student {
     public NonPlayableStudent(String name, Location location, String dialogue, String section){
