d8f775712da52ae54719ef1104a64ec767be49c8
seananabread <seevi2728@gmail.com>
2024-12-18 19:19:33 +0800

    Create InteriorElement.java

---
+++ InteriorElement.java
@@ -0,0 +1,31 @@
+package q2aa2_tau_chavez_sevilla;
+
+import java.util.ArrayList;
+
+public class InteriorElement implements Interactive{
+    private String name, dialogue;
+    protected static ArrayList<InteriorElement> interiorElementList = new ArrayList<InteriorElement>();
+    
+    public InteriorElement(String name, String dialogue){
+        this.name = name;
+        this.dialogue = dialogue; 
+        interiorElementList.add(this);
+    }
+  
+    public String getName(){
+        return name;
+    }
+    
+    public ArrayList<InteriorElement> getInteriorElementList(){
+        return interiorElementList;
+    }
+    
+    public void setName(String n){
+        name = n;
+    }
+    
+    @Override
+    public void interact(){
+        System.out.println(dialogue);
+    }
+}
